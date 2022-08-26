a53eb4db4d669625b37013c65368dd164a2c2a32
kizunaiix <yaodong.sun@outlook.com>
2022-08-26 13:58:13 +0200

    second cmt

--- src/test/java/org/example/TestDog1.java
+++ src/test/java/org/example/TestDog1.java
@@ -8,6 +8,7 @@ public class TestDog1 {
     public void testX1(){
         Dog d1 = new Dog();
         d1.setColor("black");
+        d1.color = "blue";
         System.out.println(d1.showColor());
     }
 }
